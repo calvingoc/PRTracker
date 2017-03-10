717f3a8b88f7bbe504f7c160062e7108bef02555
calvingoc <calgoc@gmail.com>
2017-03-10 15:46:45 -0600

    added strings for all lifts

--- app/src/main/java/online/cagocapps/prtracker/Data/ProfileContract.java
+++ app/src/main/java/online/cagocapps/prtracker/Data/ProfileContract.java
@@ -19,6 +19,12 @@ public class ProfileContract {
     public static final class BarbellLifts implements BaseColumns{
         public static final String TABLE_NAME = "barbellLifts";
         public static final String USER_ID = "userID";
+        public static final String REPS = "reps";
+        public static final String WEIGHT = "weight";
+        public static final String ADJUSTED_ONE_REP_MAX = "adjustedOneRepMax";
+        public static final String LIFT = "lift";
+        public static final String DATE = "date";
+        public static final String COMMENTS = "comments";
 
 
         public static final String SQUAT_SNATCH = "squatSnatch";
@@ -43,23 +49,31 @@ public class ProfileContract {
         public static final String DEADLIFT = "deadlift";
         public static final String CURL = "Curl";
         public static final String RDL = "RDL";
-
-
-        public static final String REPS = "reps";
-        public static final String WEIGHT = "weight";
-        public static final String ADJUSTED_ONE_REP_MAX = "adjustedOneRepMax";
-        public static final String LIFT = "lift";
-        public static final String DATE = "date";
+        public static final String ROW = "Row";
     }
 
     public static final class DumbbellLifts implements BaseColumns{
-        public static final String TABLE_NAME = "dumbellLifts";
+        public static final String TABLE_NAME = "dumbbellLifts";
         public static final String USER_ID = "userID";
         public static final String LIFT = "lift";
         public static final String REPS = "reps";
         public static final String WEIGHT = "weight";
         public static final String ADJUSTED_ONE_REP_MAX = "adjustedOneRepMax";
         public static final String DATE = "date";
+        public static final String COMMENTS = "comments";
+
+
+        public static final String SQUAT_SNATCH = "squatSnatch";
+        public static final String BENCH = "bench";
+        public static final String INCLINE_BENCH = "incline_bench";
+        public static final String DECLINE_BENCH = "decline_bench";
+        public static final String SKULL_CRUSHER = "skullCrusher";
+        public static final String SHOULDER_PRESS ="shoulderPress";
+        public static final String POWER_CLEAN = "powerClean";
+        public static final String TRI_EXTENSION = "triExtension";
+        public static final String ROW = "Row";
+        public static final String GOBLET_SQUAT = "gobletSquat";
+        public static final String CURL = "Curl";
     }
 
     public static final class CrossFitStandards implements BaseColumns{
@@ -71,6 +85,11 @@ public class ProfileContract {
         public static final String REPS = "reps";
         public static final String TIME = "time";
         public static final String DATE = "date";
+        public static final String COMMENTS = "comments";
+
+
+        public static final String FRAN = "fran";
+        public static final String CINDY = "cindy";
     }
 
     public static final class Gymnastics implements BaseColumns{
@@ -80,6 +99,7 @@ public class ProfileContract {
         public static final String ROUNDS = "rounds";
         public static final String REPS = "reps";
         public static final String DATE = "date";
+        public static final String COMMENTS = "comments";
     }
 
     public static final class Running implements BaseColumns{
@@ -88,6 +108,7 @@ public class ProfileContract {
         public static final String DISTANCE = "distance";
         public static final String TIME = "time";
         public static final String DATE = "date";
+        public static final String COMMENTS = "comments";
     }
 
     public static final class Swimming implements BaseColumns{
@@ -97,5 +118,6 @@ public class ProfileContract {
         public static final String DISTANCE = "distance";
         public static final String TIME = "time";
         public static final String DATE = "date";
+        public static final String COMMENTS = "comments";
     }
 }
