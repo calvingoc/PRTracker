2778cf5d3177b01316ddae1d700fb1da3cfdca22
calvingoc <calgoc@gmail.com>
2017-03-29 20:12:29 -0500

    building out commresults activity

--- app/src/main/java/online/cagocapps/prtracker/CommunityResults.java
+++ app/src/main/java/online/cagocapps/prtracker/CommunityResults.java
@@ -1,5 +1,8 @@
 package online.cagocapps.prtracker;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +11,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 import online.cagocapps.prtracker.Data.ProfileContract;
+import online.cagocapps.prtracker.Data.ProfileDBHelper;
+import online.cagocapps.prtracker.Data.ResultObject;
 
 public class CommunityResults extends AppCompatActivity {
     private Spinner spinActivity;
@@ -23,11 +37,22 @@ public class CommunityResults extends AppCompatActivity {
     private TextView tvCommBest;
     private TextView tvCommAverage;
     private TextView tvStandDev;
+    private String tableName;
+    private String compareColumn;
+
+    private ProfileDBHelper dbHelper;
+    private SQLiteDatabase dbWrite;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_results);
+
+        dbHelper = new ProfileDBHelper(this);
+        dbWrite = dbHelper.getWritableDatabase();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         spinActivity = (Spinner) findViewById(R.id.cr_spin_activity);
         spinCategory = (Spinner) findViewById(R.id.cr_spin_category);
         spinGender = (Spinner) findViewById(R.id.cr_spin_gender);
@@ -57,9 +82,7 @@ public class CommunityResults extends AppCompatActivity {
                             @Override
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                                 compareColumn = ProfileContract.BarbellLifts.ADJUSTED_ONE_REP_MAX;
-                                percentileValue.setText("");
-                                prValue.setText("");
-                                weightBased();
+                                updatePage();
                             }
 
                             @Override
@@ -80,10 +103,7 @@ public class CommunityResults extends AppCompatActivity {
                             @Override
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                                 compareColumn = ProfileContract.BarbellLifts.ADJUSTED_ONE_REP_MAX;
-                                percentileValue.setText("");
-                                prValue.setText("");
-                                weightBased();
-                                plank = false;
+                                updatePage();
                             }
 
                             @Override
@@ -105,16 +125,11 @@ public class CommunityResults extends AppCompatActivity {
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                                 if (i == 16) {
                                     compareColumn =ProfileContract.Running.TIME;
-                                    percentileValue.setText("");
-                                    prValue.setText("");
-                                    weightBased();
-
-                                    plank = true;
+                                    updatePage();
                                 }
                                 else{
                                     compareColumn = ProfileContract.BarbellLifts.REPS;
-                                    weightBased();
-                                    plank = false;
+                                    updatePage();
                                 }
 
                             }
@@ -137,10 +152,7 @@ public class CommunityResults extends AppCompatActivity {
                             @Override
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                                 compareColumn = ProfileContract.Running.TIME;
-                                percentileValue.setText("");
-                                prValue.setText("");
-                                weightBased();
-                                plank = false;
+                                updatePage();
                             }
 
                             @Override
@@ -161,10 +173,7 @@ public class CommunityResults extends AppCompatActivity {
                             @Override
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                                 compareColumn = ProfileContract.Running.TIME;
-                                percentileValue.setText("");
-                                prValue.setText("");
-                                weightBased();
-                                plank = false;
+                                updatePage();
                             }
 
                             @Override
@@ -187,16 +196,11 @@ public class CommunityResults extends AppCompatActivity {
                                 if (i == 0 || i == 2 || i == 3 || i == 4
                                         || i == 5 || i == 6 || i == 7 || i == 8 || i == 9){
                                     compareColumn = ProfileContract.Running.TIME;
-                                    percentileValue.setText("");
-                                    prValue.setText("");
-                                    weightBased();
+                                    updatePage();
                                 }else {
                                     compareColumn = ProfileContract.BarbellLifts.REPS;
-                                    percentileValue.setText("");
-                                    prValue.setText("");
-                                    weightBased();
+                                    updatePage();
                                 }
-                                plank = false;
                             }
 
                             @Override
@@ -217,16 +221,11 @@ public class CommunityResults extends AppCompatActivity {
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                                 if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 6) {
                                     compareColumn = ProfileContract.Running.TIME;
-                                    percentileValue.setText("");
-                                    prValue.setText("");
-                                    weightBased();
+                                    updatePage();
                                 }else {
                                     compareColumn = ProfileContract.BarbellLifts.REPS;
-                                    percentileValue.setText("");
-                                    prValue.setText("");
-                                    weightBased();
+                                    updatePage();
                                 }
-                                plank = false;
                             }
 
                             @Override
@@ -247,16 +246,11 @@ public class CommunityResults extends AppCompatActivity {
                             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                                 if (i == 14 || i == 18 || i == 23 || i == 25) {
                                     compareColumn = ProfileContract.Running.TIME;
-                                    percentileValue.setText("");
-                                    prValue.setText("");
-                                    weightBased();
+                                    updatePage();
                                 }else {
                                     compareColumn = ProfileContract.BarbellLifts.REPS;
-                                    percentileValue.setText("");
-                                    prValue.setText("");
-                                    weightBased();
+                                    updatePage();
                                 }
-                                plank = false;
                             }
 
                             @Override
@@ -274,5 +268,148 @@ public class CommunityResults extends AppCompatActivity {
 
             }
         });
+        spinGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updatePage();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinSkill.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updatePage();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinWeight.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updatePage();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinAge.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updatePage();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinYears.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updatePage();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+    private void updatePage(){
+        final String userID = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.sp_userID), null);
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Cursor prCursor = dbWrite.query(
+                        ProfileContract.ProfileValues.TABLE_NAME,
+                        null,
+                        ProfileContract.ProfileValues._ID + " = ?",
+                        new String[]{userID},
+                        null,
+                        null,
+                        null
+                );
+                if (prCursor.moveToFirst()) {
+                    String birthdate = prCursor.getString(prCursor.getColumnIndex(ProfileContract.ProfileValues.BIRTHDATE));
+                    Date now = new Date();
+                    int year = now.getYear() + 1900;
+                    int age = year - Integer.valueOf(birthdate);
+                    int topWeight = 99999;
+                    int botWeight = 0;
+                    if (spinWeight.getSelectedItem().toString().contains("-") ){
+                        String[] weights = spinWeight.getSelectedItem().toString().split("-");
+                        botWeight = Integer.valueOf(weights[0])/10;
+                        topWeight = Integer.valueOf(weights[1])/10;
+                    } else if (spinWeight.getSelectedItem().toString().contains("+")){
+                        botWeight = 23;
+                    }
+                    String yearsAct = (Integer.toString(prCursor.getInt(prCursor.getColumnIndex(ProfileContract.ProfileValues.YEARS_ACTIVE))));
+                    String skill = String.valueOf(spinSkill.getSelectedItem().toString().charAt(0));
+                    int gender = -1;
+                    if (spinGender.getSelectedItem().toString().equals(getString(R.string.male))){
+                        gender = 0;
+                    } else if (spinGender.getSelectedItem().toString().equals("Female")){
+                        gender = 1;
+                    }
+                    String email = prCursor.getString(prCursor.getColumnIndex(ProfileContract.ProfileValues.EMAIL));
+                    email = email.replace(".","");
+                    prCursor.close();
+                    double pr = 0;
+                    double below = 0.0;
+                    double total = 0.0;
+                    double equal = 0.0;
+                    ArrayList<Integer> results = new ArrayList<Integer>();
+                    for (DataSnapshot genderData : dataSnapshot.child(spinActivity.getSelectedItem().toString()).getChildren()) {
+                        if(Integer.valueOf(genderData.getKey()) == gender ||
+                                gender == -1) {
+                            for(DataSnapshot skillData : genderData.getChildren()){
+                                if (skillData.getKey().equals(skill) || skill.equals("A")){
+                                    for(DataSnapshot weightData : skillData.getChildren() ){
+                                        if (Integer.valueOf(weightData.getKey()) >= botWeight && Integer.valueOf(weightData.getKey()) <= topWeight){
+
+                                        }
+                                    }
+                                }
+                        }
+                        ResultObject resultObject = comResult.getValue(ResultObject.class);
+                        results.add(resultObject.getResult());
+                        String curEmail = comResult.getKey();
+                        if (curEmail.equals(email)) {
+                            pr = resultObject.getResult();
+                        }
+                    }
+                    for (int i : results) {
+                        if (i < pr) {
+                            below++;
+                            total++;
+                        } else if (i == pr) {
+                            equal++;
+                            total++;
+                        } else total++;
+                    }
+                    tvPercentile.setText(Double.toString((below + .5 * equal * 100) / total));
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        }
+
+    }
+
 }
